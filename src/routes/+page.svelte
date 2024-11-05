bf768a45294b5c8b1cd260e911fcc02fdd5066d7
Jacob Hart <jacob@uc12825.ufralc.mb-per.uhb.fr>
2024-11-05 15:32:06 +0100

    modif

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit, this has changed</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>New content</p>
+<p>Anothe rmodification</p>
