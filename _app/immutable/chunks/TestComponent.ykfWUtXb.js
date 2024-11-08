39548d19fa27f316d8a7d9f71cda31ef7c008397
Jacob Hart <jacob@uc12825.ufralc.mb-per.uhb.fr>
2024-11-08 16:32:08 +0100

    add breadcrumbs

---
+++ _app/immutable/chunks/TestComponent.ykfWUtXb.js
@@ -0,0 +1,1 @@
+import{a as o,t as e}from"./disclose-version.C9obmkXr.js";var p=e("<h1>TEST COMPONENT</h1>");function n(t){var a=p();o(t,a)}export{n as T};
---
+++ _app/immutable/entry/start.CeiNJrtQ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DyDt5CkV.js";export{t as start};
